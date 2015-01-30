85253cd42657c30f615449aa9c1eaf78d2244e88
moozzyk <pawelka@microsoft.com>
2015-01-30 12:53:43 -0800

    Adding include directory for public headers

--- src/JabbrClientCpp/stdafx.h
+++ src/JabbrClientCpp/stdafx.h
@@ -1,7 +1,3 @@
-// stdafx.h : include file for standard system include files,
-// or project specific include files that are used frequently, but
-// are changed infrequently
-//
 
 #pragma once
 
@@ -10,7 +6,3 @@
 #define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
 // Windows Header Files:
 #include <windows.h>
-
-
-
-// TODO: reference additional headers your program requires here
